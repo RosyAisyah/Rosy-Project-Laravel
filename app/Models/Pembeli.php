3855ad96fd8f4e313b938155f011239055711db6
Your Name <rosynately@gmail.com>
2024-11-15 11:12:07 +0700

    filamentt

---
+++ app/Models/Pembeli.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Pembeli extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'produk_id'
+    ];
+
+    public function produk(): BelongsTo
+    {
+        return $this->belongsTo(Produk::class);
+    }
+}
+
